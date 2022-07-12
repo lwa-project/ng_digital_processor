724742fdafebbce6c151ba67014f7e8ebbcf9f36
jaycedowell <jayce.dowell@gmail.com>
2022-07-11 18:57:55 -0600

    Move to 196 MHz for the clock.

--- scripts/valon_program_196MHz.py
+++ scripts/valon_program_196MHz.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 	#synth.set_label(valon.SYNTH_A, "Sampling clock") # Note: 16 char limit
 	#synth.set_label(valon.SYNTH_B, "Tone injection") # Note: 16 char limit
 	print("Old synth A freq:", synth.get_frequency(valon.SYNTH_A))
-	synth.set_frequency(valon.SYNTH_A, 196.608, 0.008)
+	synth.set_frequency(valon.SYNTH_A, 196.0, 0.0)
 	print("New synth A freq:", synth.get_frequency(valon.SYNTH_A))
 	synth.set_rf_level(valon.SYNTH_A, 5)
 	synth.flash()
