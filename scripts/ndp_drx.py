67930d790e4711a8e79a9409a0d746b215c9efad
jaycedowell <jaycedowell@gmail.com>
2023-08-30 10:17:08 -0600

    TODO: Deal with what the new TBF means in Bifrost.

--- scripts/ndp_drx.py
+++ scripts/ndp_drx.py
@@ -282,7 +282,7 @@ class TriggeredDumpOp(object):
         #self.iring.resize(self.ntime_gulp*frame_nbyte_max,
         #                  self.ntime_buf *frame_nbyte_max)
                           
-        self.udt = UDPTransmit('tbf_%i' % (ninput_max//2,), sock=self.sock, core=self.core)
+        self.udt = UDPTransmit('tbf', sock=self.sock, core=self.core)
         self.desc = HeaderInfo()
         while not self.iring.writing_ended():
             config = self.configMessage(block=False)
