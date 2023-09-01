24d7ffadbf083bff8467159ccdaab803f2505937
jaycedowell <jaycedowell@gmail.com>
2023-09-01 15:49:42 -0600

    Another typo.

--- scripts/ndp/Ndp.py
+++ scripts/ndp/Ndp.py
@@ -77,7 +77,7 @@ class SlotCommandProcessor(object):
 
 class DrxCommand(object):
     def __init__(self, msg):
-        if isistance(msg, str):
+        if isinstance(msg, str):
             msg = msg.encode()
         self.beam, self.tuning, self.freq, self.filt, self.gain, self.subslot \
             = struct.unpack('>BBfBhB', msg.data)
