14811c659b7133adc68dac4c6c8fa0858fdb9ea1
jaycedowell <jayce.dowell@gmail.com>
2023-09-01 16:49:44 -0600

    Disable the FIR filter for now.

--- scripts/ndp_tengine.py
+++ scripts/ndp_tengine.py
@@ -629,13 +629,14 @@ class TEngineOp(object):
                                 
                                 ## FIR filter
                                 try:
-                                    bfir.execute(gdata, fdata)
+                                    #bfir.execute(gdata, fdata)
+                                    copy_array(fdata, gdata)
                                 except NameError:
                                     fdata = BFArray(shape=gdata.shape, dtype=gdata.dtype, space='cuda')
                                     
                                     bfir = Fir()
-                                    bfir.init(self.coeffs, 1)
-                                    bfir.execute(gdata, fdata)
+                                    #bfir.init(self.coeffs, 1)
+                                    #bfir.execute(gdata, fdata)
                                     
                                 ## Quantization
                                 try:
