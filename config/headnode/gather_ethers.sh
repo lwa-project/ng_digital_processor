fd150cf476d9fe15c22cc8564c3ba33e1ada56ad
jaycedowell <jayce.dowell@gmail.com>
2025-05-30 11:35:11 -0600

    Add in ndp0 MACs as well and remove duplicates.

--- config/headnode/gather_ethers.sh
+++ config/headnode/gather_ethers.sh
@@ -15,15 +15,24 @@ if [[ ! -f ${filename} ]]; then
 	exit
 fi
 
+ips=$(grep ndp0-data ${filename} | awk '{print $1}')
+macs0=""
+for ip in ${ips}; do
+  hname=$(grep ${ip} ${filename} | awk '{print $2}')
+  iface=$(ip addr show to ${ip} | head -n1 | awk '{print $2}' | sed -e "s/:.*//g;")
+  mac=$(ip link show ${iface} | grep ether | awk '{print $2}')
+  echo "${mac} ${hname}"
+done
+
 ips=$(grep ndp ${filename} | grep data | awk '{print $1}')
 for ip in ${ips}; do
   ping -c1 ${ip} > /dev/null 2> /dev/null
 done
 
-macs=$(arp -v | grep ndp | grep data | sort)
+macs=$(arp -v | grep ndp | grep data | awk '{print $1,$3}' | sort | uniq)
 old_IFS=${IFS}
 IFS=$'\n'
 for mac in ${macs}; do
-  echo ${mac} | awk '{print $3,$1}'
+  echo ${mac} | awk '{print $2,$1}'
 done
 IFS=${old_IFS}
