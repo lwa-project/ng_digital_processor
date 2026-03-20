c869c43a34110b03fd55381a449244316c9ab9df
jaycedowell <jayce.dowell@gmail.com>
2026-03-20 08:39:21 -0600

    Use the config file to see what we actually need to check.

--- scripts/ndp_log_gpu_temps.py
+++ scripts/ndp_log_gpu_temps.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python3
 
-import os
 import sys
+sys.path.append('/usr/local/bin')
+
 import time
 import subprocess
 
+from ndp import NdpConfig
+
 
 if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Usage:", sys.argv[0], "config_file")
+        sys.exit(-1)
+        
+    config_filename = sys.argv[1]
+    config = NdpConfig.parse_config_file(config_filename)
+    
+    nvidia_cmd = ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader,nounits']
+    
     data = [time.time(),]
-    for server in range(0, 4+1):
-        cmd = []
-        if server != 0:
-            cmd.extend(['ssh', 'ndp%i' % server])
-        cmd.extend(['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader,nounits'])
+    
+    # Head node (local)
+    try:
+        subdata = []
+        output = subprocess.check_output(nvidia_cmd, text=True)
+        for line in output.split('\n')[:-1]:
+            subdata.append(float(line))
+        data.extend(subdata)
+    except Exception as e:
+        print("WARNING: head node -> %s" % (e,))
+        data.extend([-99.0 for i in range(2)])
+        
+    # Compute servers (remote)
+    for server in config['host']['servers']:
+        cmd = ['ssh', server] + nvidia_cmd
         
         try:
             subdata = []
             output = subprocess.check_output(cmd, text=True)
             for line in output.split('\n')[:-1]:
-                value = float(line)
-                subdata.append(value)
+                subdata.append(float(line))
             data.extend(subdata)
-            
         except Exception as e:
-            print("WARNING: %i -> %s" % (server, e))
+            print("WARNING: %s -> %s" % (server, e))
             data.extend([-99.0 for i in range(2)])
-
-    with open('/home/ndp/log/gpu.txt', 'a') as fh:
+            
+    filename = config['log']['files']['server_temps']
+    with open(filename, 'a') as fh:
         fh.write(','.join([str(v) for v in data]))
         fh.write('\n')
