2ea4f475de3f1bac2e10b1d095315f960d3cdc03
jaycedowell <jayce.dowell@gmail.com>
2026-09-26 08:46:48 -0600

    Generalize and fix a typo.

--- scripts/uploadLogfile.py
+++ scripts/uploadLogfile.py
@@ -10,13 +10,13 @@ from lwa_auth import KEYS as LWA_AUTH_KEYS
 from lwa_auth.signed_requests import post as signed_post
 
 URL = "https://lda10g.alliance.unm.edu/metadata/sorter/upload"
-SITE = "lwana"
+SITE = gethostname().split('-', 1)[0]
 TYPE = "SSLOG"
 
 # Send the update to lwalab
 r = os.path.realpath(sys.argv[1])
 f = signed_post(LWA_AUTH_KEYS.get('ndp', kind='private'), URL,
                 data={'site': SITE, 'type': TYPE, 'subsystem': 'NDP'},
-                files={'file': open(r, 'rb'))
+                files={'file': open(r, 'rb')})
 print(f.text)
 f.close()
